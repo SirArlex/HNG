c247d52dffba5459a301f48b60b9a1e4c404530a
SirArlex <88986261+SirArlex@users.noreply.github.com>
2023-09-11 05:29:38 -0700

    Create app.js
    hng task 1

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const moment = require("moment");
+
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.listen(3000)
+
+
+
+app.get('/api', (req, res) => 
+{
+    const slack_name = req.query.slack_name;
+  const track = req.query.track;
+
+    
+    const slackDetails ={
+        'slack_name': slack_name,
+       "current_day": moment().format("dddd"),
+       utc_time: moment.utc().format("YYYY-MM-DDTHH:mm:ss[Z]"),
+        'track': track,
+        'github_file-url': '',
+        'github-repo_url': "",
+        'status_code' : 200
+
+    }
+    res.status(200).json(slackDetails)
+});
+
+
+
